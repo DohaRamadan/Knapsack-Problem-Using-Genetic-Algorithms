07fcae0109b2d8f87cddd5b47ab2f44010166f64
DohaRamadan <77820526+DohaRamadan@users.noreply.github.com>
2022-11-09 15:49:58 +0200

    Initial

---
+++ cmake-build-debug/CMakeFiles/SoftComputingAss1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoftComputingAss1.dir/main.cpp.obj"
+  "CMakeFiles/SoftComputingAss1.dir/main.cpp.obj.d"
+  "SoftComputingAss1.exe"
+  "SoftComputingAss1.exe.manifest"
+  "SoftComputingAss1.pdb"
+  "libSoftComputingAss1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SoftComputingAss1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SoftComputingAss1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SoftComputingAss1.
+# This may be replaced when dependencies are built.
