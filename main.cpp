91b917f7929910cf01870913fecbc4af61f16ce0
DohaRamadan <77820526+DohaRamadan@users.noreply.github.com>
2022-11-09 20:23:19 +0200

    refacrtoring & fixed some bugs

--- main.cpp
+++ main.cpp
@@ -4,34 +4,42 @@
 #define value second
 using namespace std;
 
-vector<pair<int, int>> weights;
-// pair of chromosome and it's fitness
-vector<pair<vector<int>, int>> currentGeneration, offsprings;
-int knapscakWeight, itemsNum;
-const int POP_SIZE = 10, MAX_GENERATION = 20;
-const float PC = 0.6, PM = 0.1;
 
-bool sortByFitness(pair<vector<int>, int>& a, pair<vector<int>, int>& b){
-    return a.second > b.second;
+struct Individual{
+    vector<int> chromosome;
+    int fitness;
+    int totalWeight=0, totalValue = 0;
+};
+
+bool sortByFitness(Individual& a, Individual& b){
+    return a.fitness > b.fitness;
 }
 
-vector<pair<vector<int>, int>> InitializePopulation(){
-    vector<pair<vector<int>, int>> newGeneration;
+vector<pair<int, int>> weights;
+vector<Individual> currentGeneration, offsprings;
+int knapscakWeight, itemsNum;
+const int POP_SIZE = 20, MAX_GENERATION = 30;
+const float PC = 0.6, PM = 0.1;
+
+vector<Individual> InitializePopulation(){
+    vector<Individual> newGeneration;
     while(newGeneration.size() < POP_SIZE){
-        vector<int> chromosome(itemsNum);
+        Individual v;
+        v.chromosome.resize(itemsNum);
         int weightSum = 0;
         for (int i = 0; i < weights.size(); ++i) {
             // generating a random float between 1 and 0
             float r = (float)rand() / (float)RAND_MAX;
             if(r <= 0.5){
-                chromosome[i] = 0;
+                v.chromosome[i] = 0;
             }else{
-                chromosome[i] = 1;
-                weightSum += weights[i].weight;
+                v.chromosome[i] = 1;
+                v.totalValue += weights[i].value;
+                v.totalWeight += weights[i].weight;
             }
         }
-        if(weightSum <= knapscakWeight){
-            newGeneration.push_back({chromosome, -1});
+        if(v.totalWeight <= knapscakWeight){
+            newGeneration.push_back(v);
         }
     }
     return newGeneration;
@@ -39,31 +47,42 @@ vector<pair<vector<int>, int>> InitializePopulation(){
 
 void evaluateFitness(){
     for (int i = 0; i < POP_SIZE; ++i) {
-        int totalValue = 0, totalWeight = 0;
-        for (int j = 0; j < itemsNum; ++j) {
-            if(currentGeneration[i].first[j]){
-                totalWeight += weights[j].weight;
-                totalValue += weights[j].value;
-            }
-        }
-        if(totalWeight > knapscakWeight){
-            currentGeneration[i].second = 0;
+        if(currentGeneration[i].totalWeight <= knapscakWeight){
+            currentGeneration[i].fitness = currentGeneration[i].totalValue;
         }else{
-            currentGeneration[i].second = totalValue;
+            currentGeneration[i].fitness = 0;
         }
     }
-}
 
-pair<vector<int>, vector<int>> crossover(vector<int> parent1, vector<int> parent2){
+}
+pair<Individual, Individual> crossover(Individual parent1, Individual parent2){
     int r1 = 1 + (rand() % itemsNum - 1);
-    vector<int> offspring1(itemsNum), offspring2(itemsNum);
+    Individual offspring1, offspring2;
+    offspring1.chromosome.resize(itemsNum);
+    offspring2.chromosome.resize(itemsNum);
     for (int i = 0; i <= r1; ++i) {
-        offspring1[i] = parent1[i];
-        offspring2[i] = parent2[i];
+        offspring1.chromosome[i] = parent1.chromosome[i];
+        offspring2.chromosome[i] = parent2.chromosome[i];
+        if(offspring1.chromosome[i]){
+            offspring1.totalWeight += weights[i].weight;
+            offspring1.totalValue  += weights[i].value;
+        }
+        if(offspring2.chromosome[i]){
+            offspring2.totalWeight += weights[i].weight;
+            offspring2.totalValue  += weights[i].value;
+        }
     }
     for (int i = r1+1; i < itemsNum; ++i) {
-        offspring1[i] = parent2[i];
-        offspring2[i] = parent1[i];
+        offspring1.chromosome[i] = parent2.chromosome[i];
+        offspring2.chromosome[i] = parent1.chromosome[i];
+        if(offspring1.chromosome[i]){
+            offspring1.totalWeight += weights[i].weight;
+            offspring1.totalValue  += weights[i].value;
+        }
+        if(offspring2.chromosome[i]){
+            offspring2.totalWeight += weights[i].weight;
+            offspring2.totalValue  += weights[i].value;
+        }
     }
     return {offspring1, offspring2};
 }
@@ -74,8 +93,15 @@ void Mutation(){
             // generating a random float between 1 and 0
             float ri = (float)rand() / (float)RAND_MAX;
             if(ri <= PM){
-                if(offsprings[i].first[j]) offsprings[i].first[j] = 0;
-                else offsprings[i].first[j] = 1;
+                if(offsprings[i].chromosome[j]){
+                    offsprings[i].chromosome[j] = 0;
+                    offsprings[i].totalWeight -= weights[j].weight;
+                    offsprings[i].totalValue -= weights[j].value;
+                } else{
+                    offsprings[i].chromosome[j] = 1;
+                    offsprings[i].totalWeight += weights[j].weight;
+                    offsprings[i].totalValue += weights[j].value;
+                }
             }
         }
     }
@@ -84,23 +110,23 @@ void Mutation(){
 
 void RouletteWheelSelection(){
     int totalFitness = 0;
-    vector<int> cumlative_fitness(POP_SIZE);
+    vector<int> cumulative_fitness(POP_SIZE);
     for (int i = 0; i < POP_SIZE; ++i) {
-        totalFitness += currentGeneration[i].second;
-        cumlative_fitness[i] = totalFitness;
+        totalFitness += currentGeneration[i].fitness;
+        cumulative_fitness[i] = totalFitness;
     }
     offsprings.clear();
     while (offsprings.size() < POP_SIZE) {
         int r1 =  (rand() % (totalFitness + 1));
         int r2 =  (rand() % (totalFitness + 1));
-        int parent1 = lower_bound(cumlative_fitness.begin(), cumlative_fitness.end(), r1) - cumlative_fitness.begin();
-        int parent2 = lower_bound(cumlative_fitness.begin(), cumlative_fitness.end(), r2) - cumlative_fitness.begin();
+        int parent1 = lower_bound(cumulative_fitness.begin(), cumulative_fitness.end(), r1) - cumulative_fitness.begin();
+        int parent2 = lower_bound(cumulative_fitness.begin(), cumulative_fitness.end(), r2) - cumulative_fitness.begin();
         // generating a random float between 1 and 0
         float r = (float)rand() / (float)RAND_MAX;
         if(r <= PC){
-            pair<vector<int>, vector<int>> springs = crossover(currentGeneration[parent1].first, currentGeneration[parent2].first);
-            offsprings.push_back({springs.first, -1});
-            offsprings.push_back({springs.second, -1});
+            pair<Individual, Individual> springs = crossover(currentGeneration[parent1], currentGeneration[parent2]);
+            offsprings.push_back(springs.first);
+            offsprings.push_back(springs.second);
         }else{
             offsprings.push_back(currentGeneration[parent1]);
             offsprings.push_back(currentGeneration[parent2]);
@@ -108,10 +134,10 @@ void RouletteWheelSelection(){
     }
 }
 
-
-pair<vector<int>, int> GA(){
+Individual GA(){
     cin >> knapscakWeight >> itemsNum;
     weights.resize(itemsNum);
+    currentGeneration.resize(POP_SIZE);
     for (int i = 0; i < itemsNum; ++i) {
         cin >> weights[i].weight >> weights[i].value;
     }
@@ -122,8 +148,9 @@ pair<vector<int>, int> GA(){
         Mutation();
         currentGeneration = offsprings;
     }
+    evaluateFitness();
     std::sort(currentGeneration.begin(), currentGeneration.end(), sortByFitness);
-    pair<vector<int>, int> bestIndividual = currentGeneration[0];
+    Individual bestIndividual = currentGeneration[0];
     return bestIndividual;
 }
 
@@ -132,17 +159,17 @@ int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
-        pair<vector<int>, int> bestIndividual = GA();
+        Individual bestIndividual = GA();
         int selectedItems = 0;
-        for (int j = 0; j < bestIndividual.first.size(); ++j) {
-            if(bestIndividual.first[j] == 1){
+        for (int j = 0; j < bestIndividual.chromosome.size(); ++j) {
+            if(bestIndividual.chromosome[j] == 1){
                 selectedItems++;
             }
         }
-        cout << "test case " << i+1 << ": " << "number of selected items: " << selectedItems << ", total value: " << bestIndividual.second << "\n";
+        cout << "test case " << i+1 << ": " << "number of selected items: " << selectedItems << ", total value: " << bestIndividual.totalValue << "\n";
         cout << "weight and value of each selected item: \n";
-        for (int j = 0; j < bestIndividual.first.size(); ++j) {
-            if(bestIndividual.first[j] == 1){
+        for (int j = 0; j < bestIndividual.chromosome.size(); ++j) {
+            if(bestIndividual.chromosome[j] == 1){
                 cout << weights[j].weight << " " << weights[j].value << "\n";
             }
         }
